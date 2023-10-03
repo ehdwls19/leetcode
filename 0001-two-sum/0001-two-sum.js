329d3e275983cde84e5a1acaa55e69ed4d1ce130
ehdwls19 <39150608+ehdwls19@users.noreply.github.com>
2023-10-04 05:58:46 +0900

    Time: 55 ms (80.97%), Space: 42.4 MB (63.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    //set Target Int for a num[i]
-    let targetInt = 0;
-    for(let i =0; i<nums.length; i++){
-        targetInt = target-nums[i];
-        for(let j =i+1;j<nums.length;j++){
-            if(targetInt==nums[j]){
-                return [i,j];
-            }
+    let numObj = {};
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (numObj[complement] !== undefined) {
+          return [numObj[complement], i];
         }
+        numObj[nums[i]] = i;
     }
 };
