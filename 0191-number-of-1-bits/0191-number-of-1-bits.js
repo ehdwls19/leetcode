c575a141d9e66180ee30203e0a3e164985973c4a
ehdwls19 <39150608+ehdwls19@users.noreply.github.com>
2023-10-19 21:38:30 -0500

    Time: 47 ms (92.48%), Space: 42.5 MB (44.11%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.js
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -3,5 +3,13 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    return n.toString(2).replaceAll("0", "").length;    
+    let count = 0;
+    while (n !== 0) {
+        const bitComparison = n & 1; // 1 & 1 will return 1. 0 & 1 will return 0.
+        if (bitComparison === 1) {
+            count++;
+        }
+        n >>>= 1; // unsigned right shift assignment (chop off the last bit and assign it)
+    }  
+    return count;
 };
