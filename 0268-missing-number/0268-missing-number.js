595d067902185fabc71ea3975edd0e7d61ded719
ehdwls19 <39150608+ehdwls19@users.noreply.github.com>
2023-10-22 00:45:30 -0500

    Time: 62 ms (50.44%), Space: 44.1 MB (75.69%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,13 +3,5 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    nums.sort((a,b) => a-b);
-    
-    for(let i = 0; i<nums.length; i++){
-        if(i !== nums[i]){
-            return i;
-        }
-    }
-    
-    return nums.length;
+    return nums.reduce((a, c, i) => a + i + 1 - c, 0)
 };
