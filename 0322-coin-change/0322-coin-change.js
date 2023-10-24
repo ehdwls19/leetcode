544b7d81a6f0085fda7e1690d52082807c13efc7
ehdwls19 <39150608+ehdwls19@users.noreply.github.com>
2023-10-23 23:56:43 -0500

    Time: 85 ms (78.60%), Space: 47.7 MB (54.87%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    const dp = Array(amount+1).fill(Infinity);
+    dp[0] = 0;
+    
+    for(let i = 1; i < dp.length; i++) {
+        for(let coin of coins) {
+            if(i-coin >= 0) dp[i] = Math.min(dp[i], dp[i-coin]+1);
+        }
+    }
+    return dp[amount] === Infinity ? -1 : dp[amount];
+};
