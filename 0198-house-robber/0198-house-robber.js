a808df889adeb81b7ddfc521319abdad16e7be26
ehdwls19 <39150608+ehdwls19@users.noreply.github.com>
2023-10-28 23:23:30 -0600

    Time: 54 ms (36.88%), Space: 42.1 MB (22.65%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,14 +3,19 @@
  * @return {number}
  */
 var rob = function(nums) {
-    let dp = new Array(nums.length).fill(0);
-    dp[0] = nums[0];
-    dp[1] = nums[1]? nums[1] : 0;
-    for(let i = 2; i<nums.length; i++){
-        for(let j = 0; j<i-1; j++){
-            dp[i] = Math.max(dp[i],nums[i]+dp[j]);
-        }
-    }
-    return Math.max(...dp)
+    if (!nums.length) return 0;
+    if (nums.length === 1) return nums[0];
+    if (nums.length === 2) return Math.max(nums[0], nums[1]);
     
+    let maxAtTwoBefore = nums[0];
+    let maxAtOneBefore = Math.max(nums[0], nums[1]);
+    
+    for (let i = 2; i < nums.length; i++) {
+        const maxAtCurrent = Math.max(nums[i] + maxAtTwoBefore, maxAtOneBefore);
+        
+        maxAtTwoBefore = maxAtOneBefore;
+        maxAtOneBefore = maxAtCurrent;
+    }
+    
+    return maxAtOneBefore;
 };
