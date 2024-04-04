435b6e40071e21b5dae0172f8b3ccce8ea0e92f0
Dongjin Lee <39150608+dev-dongjin@users.noreply.github.com>
2024-04-04 01:52:59 -0700

    Time: 64 ms (97.37%), Space: 65.9 MB (11.77%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,15 +2,15 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
 var containsDuplicate = function(nums) {
-    if(nums.length==1){
-        return false;
-    }
-    nums.sort((a,b)=> a-b);
-    for(let i = 0; i<nums.length; i++){
-        if(nums[i] == nums[i+1]){
+    let solution = false;
+    const map = new Map();
+    for(let i = 0; i < nums.length; i++){
+        if(map.has(nums[i])){
             return true;
         }
+        map.set(nums[i],1); //{1:1, 2:1, 3:1}
     }
-    return false;
+    return solution
 };
